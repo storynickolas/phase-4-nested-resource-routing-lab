d567b842f038134798a4e931f1ea813d78b74f18
Nick Story <storynickolasl@gmail.com>
2023-02-04 13:17:53 -0500

    commit tree 88736599d1a71d83a17e3fbc5a92a6d449e621c1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,45 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+
   end
+
+  def items
+    user = User.find_by(id: params[:user_id])
+    if user
+      items = user.items
+      render json: items
+    else
+      render_not_found_response
+    end
+  end
+
+  def item_show
+    item = Item.find(params[:id])
+    if params[:user_id] == item.user_id.to_s
+      render json: item
+    else
+      render_not_found_response
+    end
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+
+  get '/users/:user_id/items', to: 'users#items'
+  get '/users/:user_id/items/:id', to: 'users#item_show'
+  post '/users/:user_id/items', to: 'users#create'
+
 end
